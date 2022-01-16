bfda9614afc626554a91378736a0b35ab6eff475
Itst4run <tarkr7@gmail.com>
2022-01-16 05:51:17 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./style.css";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact";
-
+import {BrowserRouter as Router,Routs,Route} from
+ "react-router-dom";
 export default function App() {
   return (
+    <div>
     <div>
      <Home/>
      <AboutUs/>
      <Contact/>
     </div>
+    <Router>
+      <Routs>
+        <Route>
+        <Routs>
+      </Router>
+    <div/>
   );
 }
