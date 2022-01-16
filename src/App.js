8ae18a03acad43685ce06af71c9f9895aa0f0a0e
Itst4run <tarkr7@gmail.com>
2022-01-16 14:59:57 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react'
-import "./style.css";
-import Home from "./components/Home";
-import AboutUs from "./components/AboutUs";
-import Contact from "./components/Contact";
+import './style.css';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Contact from './components/Contact';
 import {BrowserRouter as Router,Routes,Route} from
  "react-router-dom";
 export default function App() {
   return (
+    
     <div>
-    <div>
-     <Home/>
-     <AboutUs/>
-     <Contact/>
-    </div>
-    <Router>
+     <Router>
       <Routes>
-        <Route path ="/" element={<Home/>}>
-        <Route path ="/Contact" element={<Contact/>}>
-        <Route path ="/about" element={<AboutUs/>}>
+        <Route path ="/" element={<Home />}/>
+        <Route path ="/Contact" element={<Contact />}/>
+        <Route path ="/about" element={<AboutUs />}/>
         </Routes>
       </Router>
-    <div/>
+    </div>
   );
 }
