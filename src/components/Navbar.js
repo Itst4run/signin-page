0e43261bc058a7d5df90030105c7fe155a3d935d
Itst4run <tarkr7@gmail.com>
2022-01-16 23:54:43 +0530

    1 file modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,15 @@ import {Link} from 'react-router-dom';
 function Navbar(){
 return(
 <nav>
-  <Link to="/">Home</Link>
-<Link to="/about" className="link">
-  About
-</Link>
-&nbsp;&nbsp;&nbsp;
 <Link to="/">Home</Link>
+&nbsp;&nbsp;&nbsp;
+<Link to="/about" className="link-style">
+ About
+</Link>
 &nbsp;&nbsp;&nbsp;
 <Link to="/Contact">Contact</Link>
 <Link to="/Product/1">Product</Link>
-
-
 </nav>
-
 );
 }
 export default Navbar;
